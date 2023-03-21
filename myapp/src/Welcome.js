84599c893b9b4327bcafdfb327537f618e38680b
hakantuncer <hakant599@gmail.com>
2023-03-21 18:08:27 +0300

    props2

--- myapp/src/Welcome.js
+++ myapp/src/Welcome.js
@@ -4,3 +4,12 @@ export class Welcome extends React.Component {
     return <p> Welcome, {this.props.name}!</p>;
   }
 }
+
+Welcome.defaultProps = {
+  name: "User",
+};
+
+/* If no name prop is passed to the Welcome component,
+   the component will still render, but the message 
+   inside the p tag will just say "Welcome, !", with 
+   no name displayed. */
