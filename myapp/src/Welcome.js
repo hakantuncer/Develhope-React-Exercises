9d895ca2cab9b0b29f8eccee6acfe59fee0aca97
hakantuncer <hakant599@gmail.com>
2023-03-23 23:14:55 +0300

    props5

--- myapp/src/Welcome.js
+++ myapp/src/Welcome.js
@@ -1,9 +1,17 @@
 import React from "react";
+
+export class age extends React.Component {
+  render() {
+    return <span>Your age is {this.props.age}</span>;
+  }
+}
+
 export class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p> Welcome, {this.props.name}!</p>;<p>Your age is {this.props.age}</p>;
+        <p> Welcome, {this.props.name}!</p>;
+        <age age={this.props.age} />;
       </div>
     );
   }
