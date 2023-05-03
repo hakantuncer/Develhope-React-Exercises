e4ca2c12593e1a666c52c37c2fae50e0d9db506d
hakantuncer <hakant599@gmail.com>
2023-05-03 19:17:35 +0300

    stylingcomps1

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { RenderApp } from "./App";
+import "./style";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RenderApp />);
