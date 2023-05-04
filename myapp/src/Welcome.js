4ee3c10e92276eff34156d01e628c921855e342c
hakantuncer <hakant599@gmail.com>
2023-05-04 11:24:31 +0300

    FunctionComps

--- myapp/src/Welcome.js
+++ myapp/src/Welcome.js
@@ -1,28 +1,18 @@
 import React from "react";
 
-export class age extends React.Component {
-  render() {
-    const age = this.props.age;
-    return (
-      <span>{age > 18 ? `Your age is ${age}` : "You are very young!"}</span>
-    );
-  }
+function Age({ age }) {
+  return <span>{age > 18 ? `Your age is ${age}` : "You are very young!"}</span>;
 }
 
-export class Welcome extends React.Component {
-  render() {
-    const age = this.props.age;
-    const name = this.props.name;
-    return (
-      <div>
-        <p> Welcome, {name}!</p>;{age && <age age={age} />}
-      </div>
-    );
-  }
+export default function Welcome({ name = "User", age }) {
+  return (
+    <div>
+      <p> Welcome, {name}!</p>
+      {age && <Age age={age} />}
+    </div>
+  );
 }
 
 Welcome.defaultProps = {
   name: "User",
 };
-
-// I modified the code for age prop. Greater than 18, less than 65 and name is 'John'.
