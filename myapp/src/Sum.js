cdfa49462f5dcbd32e1838a7f89df8bd7025b776
hakantuncer <hakant599@gmail.com>
2023-05-04 11:31:28 +0300

    FunctionComps

--- myapp/src/Sum.js
+++ myapp/src/Sum.js
@@ -4,3 +4,10 @@ function Sum({ numbers }) {
   const sum = numbers.reduce((acc, num) => acc + num, 0);
   return <h1> {sum}</h1>;
 }
+
+Sum.defaultProps = {
+  numbers: [0, 0, 0],
+};
+
+// If the numbers prop of the Sum component is not set, it will be undefined.
+//To set a default value for the numbers prop, we can use the defaultProps property
