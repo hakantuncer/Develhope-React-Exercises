95e531ce0f831ac201142dc88f7d043d880f52eb
hakantuncer <hakant599@gmail.com>
2023-05-09 14:23:32 +0300

    useState

--- myapp/src/Login.js
+++ myapp/src/Login.js
@@ -1,54 +1,78 @@
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
 
-export class UncontrolledLogin extends Component {
-  usernameRef = React.createRef();
-  passwordRef = React.createRef();
-  rememberRef = React.createRef();
+export function UncontrolledLogin(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
 
-  componentDidMount() {
-    this.usernameRef.current.focus();
-  }
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+  const rememberRef = useRef(null);
 
-  handleLogin = () => {
-    const username = this.usernameRef.current.value;
-    const password = this.passwordRef.current.value;
-    const remember = this.rememberRef.current.checked;
+  const handleLogin = () => {
+    const usernameValue = usernameRef.current.value;
+    const passwordValue = passwordRef.current.value;
+    const rememberValue = rememberRef.current.checked;
 
-    this.props.onLogin({ username, password, remember });
+    props.onLogin({
+      username: usernameValue,
+      password: passwordValue,
+      remember: rememberValue,
+    });
   };
 
-  handleReset = () => {
-    this.usernameRef.current.value = "";
-    this.passwordRef.current.value = "";
-    this.rememberRef.current.checked = false;
+  const handleReset = () => {
+    setUsername("");
+    setPassword("");
+    setRemember(false);
+
+    usernameRef.current.value = "";
+    passwordRef.current.value = "";
+    rememberRef.current.checked = false;
   };
 
-  render() {
-    const { username, password, remember } = this.state;
-    const isLoginDisabled = username === "" || password === "";
-    const buttonStyle = {
-      backgroundColor: password.length < 8 ? "red" : "green",
-    };
-    return (
-      <div>
-        <input type="text" name="username" ref={this.usernameRef} />
+  const isLoginDisabled = username === "" || password === "";
+  const buttonStyle = {
+    backgroundColor: password.length < 8 ? "red" : "green",
+  };
 
-        <input type="password" name="password" ref={this.passwordRef} />
+  return (
+    <div>
+      <input
+        type="text"
+        name="username"
+        ref={usernameRef}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
 
-        <br />
+      <input
+        type="password"
+        name="password"
+        ref={passwordRef}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-        <input type="checkbox" name="remember" ref={this.rememberRef} />
+      <br />
 
-        <button
-          onClick={this.handleLogin}
-          style={buttonStyle}
-          disabled={isLoginDisabled}
-        >
-          Login
-        </button>
+      <input
+        type="checkbox"
+        name="remember"
+        ref={rememberRef}
+        checked={remember}
+        onChange={(e) => setRemember(e.target.checked)}
+      />
 
-        <button onClick={this.handleReset}>Reset</button>
-      </div>
-    );
-  }
+      <button
+        onClick={handleLogin}
+        style={buttonStyle}
+        disabled={isLoginDisabled}
+      >
+        Login
+      </button>
+
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 }
